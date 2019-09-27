ddd4c8b12637c1ce2c750b8eadf35ef95143519e
wzj <wzj@98c59efd-dd09-bc47-b51b-7d1fa9db0e8f>
2019-09-27 03:35:00 +0000

    git-svn-id: https://39.108.144.9:8443/svn/tdt@8 98c59efd-dd09-bc47-b51b-7d1fa9db0e8f

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -4,13 +4,13 @@ namespace App\Http\Controllers\Api;
 
 use App\Exceptions\OutputServerMessageException;
 use App\Repositories\Eloquent\PageRepositoryInterface;
-use App\Services\LBSService;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Api\BaseController;
 use App\Models\Banner;
 use App\Models\Setting;
 use Log;
 use App\Services\AmapService;
+use App\Services\LBSService;
 
 class HomeController extends BaseController
 {
@@ -22,6 +22,7 @@ class HomeController extends BaseController
             ->pushCriteria(\App\Repositories\Criteria\RequestCriteria::class)
             ->pushCriteria(\App\Repositories\Criteria\PageResourceCriteria::class);
     }
+
     public function getCoordinates(Request $request)
     {
         $address = $request->address;
@@ -39,5 +40,22 @@ class HomeController extends BaseController
             'latitude' => $location['lat'],
         ])->json();
     }
-
+    /* 高德地图
+    public function getCoordinates(Request $request)
+    {
+        $address = $request->address;
+        $amap_service = new AmapService();
+        $map_data = $amap_service->geocode_geo($address);
+        if(!isset($map_data['geocodes'][0]))
+        {
+            throw new OutputServerMessageException("请输入正确地址");
+        }
+        $location = $map_data['geocodes'][0]['location'];
+        $location_arr = explode(',',$location);
+        return $this->response->success()->data([
+            'longitude' => $location_arr[0],
+            'latitude' => $location_arr[1],
+        ])->json();
+    }
+    */
 }
