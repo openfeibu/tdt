33c0598056515420af176abb33b017468e2eca2b
wzj <wzj@98c59efd-dd09-bc47-b51b-7d1fa9db0e8f>
2019-09-28 10:18:59 +0000

    git-svn-id: https://39.108.144.9:8443/svn/tdt@10 98c59efd-dd09-bc47-b51b-7d1fa9db0e8f

--- app/Http/Controllers/Api/ShopController.php
+++ app/Http/Controllers/Api/ShopController.php
@@ -2,18 +2,21 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Exceptions\OutputServerMessageException;
 use App\Repositories\Eloquent\ShopRepositoryInterface;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Api\BaseController;
 use App\Models\User;
+use App\Models\Shop;
 use App\Services\LBSService;
 
 class ShopController extends BaseController
 {
-    public function __construct(ShopRepositoryInterface $shop)
+    public function __construct(ShopRepositoryInterface $shop,LBSService $lbs_service)
     {
         parent::__construct();
         $this->repository = $shop;
+        $this->lbs_service = $lbs_service;
     }
     public function getShops(Request $request)
     {
@@ -21,18 +24,49 @@ class ShopController extends BaseController
         $longitude = $request->longitude;
         $latitude = $request->latitude;
 
-        $lbs_service = new LBSService();
-        $map_data = $lbs_service->geocode_regeo($longitude,$latitude);
+        $map_data = $this->lbs_service->geocode_regeo($longitude,$latitude);
 
         $adcode = $map_data['result']['ad_info']['adcode'];
         $district = app('area_repository')->where('code',$adcode)->first();
-        var_dump($map_data);exit;
+
+        if(!$district)
+        {
+            throw new OutputServerMessageException("地址找不到，请联系管理员更新");
+        }
+
         $city = app('area_repository')->where('code',$district->parent_code)->first();
 
+        $city_grade = '';
 
+        $site = $city->city_grade;
         if($district->city_grade == 'county-city')
         {
+            $site = $district->city_grade;
+            $city_grade = $district->city_grade;
+        }else{
+            $city_grade = $city->city_grade;
+        }
 
+        $km = setting($city_grade);
+        $km = $km ? $km : setting('default_km');
+
+        $shops = Shop::selectRaw("*,(acos(sin((".$latitude."*3.1415)/180) * sin((latitude*3.1415)/180) + cos((".$latitude."*3.1415)/180) * cos((latitude*3.1415)/180) * cos((".$longitude."*3.1415)/180 - (longitude*3.1415)/180))*6370.996) as distance")
+            ->whereRaw("(acos(sin((".$latitude."*3.1415)/180) * sin((latitude*3.1415)/180) + cos((".$latitude."*3.1415)/180) * cos((latitude*3.1415)/180) * cos((".$longitude."*3.1415)/180 - (longitude*3.1415)/180))*6370.996) <= ".$km)
+            ->get()
+            ->toArray();
+
+        foreach ($shops as $key => $shop)
+        {
+            if($shop['latitude'] == $latitude && $shop['longitude'] =  $longitude)
+            {
+                $shops[$key]['walking_distance'] = 0;
+            }else{
+                $direction = $this->lbs_service->direction('walking',$latitude,$longitude,$shop['latitude'],$shop['longitude']);
+
+                $shops[$key]['walking_distance'] = $direction['result']['routes']['0']['distance'] ?? '未知';
+            }
+
+            $shops[$key]['distance'] = ceil($shop['distance'] * 1000);
         }
 
         User::where('id',$user->id)->update([
@@ -40,13 +74,11 @@ class ShopController extends BaseController
             'latitude' => $latitude,
         ]);
 
-        $shops = $this->repository
-            ->orderBy('id','desc')
-            ->all();
-
         return $this->response->success()->data([
-            'longitude' => $location['lng'],
-            'latitude' => $location['lat'],
+            'site' => $site,
+            'km' => $km,
+            'shops' => $shops
         ])->json();
+
     }
 }
--- app/Services/LBSService.php
+++ app/Services/LBSService.php
@@ -14,11 +14,13 @@ class LBSService
 
         $this->client = new Client();
 
+        $this->domain = "https://apis.map.qq.com/";
+
         $this->debug = false;
     }
     public function geocode_regeo($lng,$lat)
     {
-        $url = "https://apis.map.qq.com/ws/geocoder/v1/?key=".$this->key."&location=".$lat.','.$lng.'&get_poi=0';
+        $url = $this->domain."ws/geocoder/v1/?key=".$this->key."&location=".$lat.','.$lng.'&get_poi=0';
         $res = $this->client->get($url);
         $data = json_decode($res->getBody()->getContents(),true);
 
@@ -36,7 +38,7 @@ class LBSService
     }
     public function geocode_geo($address)
     {
-        $url = "https://apis.map.qq.com/ws/geocoder/v1/?key=".$this->key."&address=".$address;
+        $url = $this->domain."ws/geocoder/v1/?key=".$this->key."&address=".$address;
         $res = $this->client->get($url);
         $data = json_decode($res->getBody()->getContents(),true);
 
@@ -52,4 +54,37 @@ class LBSService
 
         return $data;
     }
+
+    /**
+     * @param $type
+     * @param $from_latitude
+     * @param $from_longitude
+     * @param $to_latitude
+     * @param $to_longitude
+     * @return array
+     * 1. 驾车（driving）：支持结合实时路况、少收费、不走高速等多种偏好，精准预估到达时间（ETA）；
+     * 2. 步行（walking）：基于步行路线规划。
+     * 3. 骑行（bicycling）：基于自行车的骑行路线；
+     * 4. 公交（transit）：支持公共汽车、地铁等多种公共交通工具的换乘方案计算；
+     */
+    public function direction($type,$from_latitude,$from_longitude,$to_latitude,$to_longitude)
+    {
+        $url = $this->domain."ws/direction/v1/".$type."?key=".$this->key."&from=".$from_latitude.','.$from_longitude."&to=".$to_latitude.','.$to_longitude;
+
+        $res = $this->client->get($url);
+        $data = json_decode($res->getBody()->getContents(),true);
+
+        if($data['status'])
+        {
+            if($this->debug)
+            {
+                $data['error'] = 1;
+                return $data;
+            }
+            throw new \App\Exceptions\OutputServerMessageException($data['message']);
+        }
+
+        return $data;
+    }
+
 }
